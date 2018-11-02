8ae20d7f59b5611a4592f14dd00c73dd979f7ff0
dmalikov <dmalikov@rdvb.ru>
2018-11-02 19:47:37 +0300

    modify and comment Symmetric Pairs solution file

--- Sql/symmetric-pairs/symmetric-pairs.sql
+++ Sql/symmetric-pairs/symmetric-pairs.sql
@@ -1,7 +1,10 @@
+--HackerRank "Symmetric Pairs"
+--How to detect unique pair of values a and b?
+--Make two arithmetic operations: + and * and partitionin by results
 ;with cte
 as
 (
-    select ROW_NUMBER() over(partition by (X + Y), abs(X - Y) order by x desc) as rn, X, Y
+    select ROW_NUMBER() over(partition by (X + Y), (X * Y) order by x desc) as rn, X, Y
     from Functions
 )
 select distinct X, Y from cte
