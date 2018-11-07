fe7ccd458c0c9d7a9ef3d3cefb74cbabaca5f909
dmalikov <dmalikov@rdvb.ru>
2018-11-07 11:17:56 +0300

    complete the-company challenge

--- Sql/the-company/the-company.sql
+++ Sql/the-company/the-company.sql
@@ -1,69 +1,32 @@
-;with cte
-as
-(
-	select distinct
-		c.company_code
-		, c.founder
-		, lm.lead_manager_code
-		, sm.senior_manager_code
-		, m.manager_code
-		, e.employee_code
-	from Company c
-		left join Lead_Manager lm on lm.company_code = c.company_code
-		left join Senior_Manager sm on sm.lead_manager_code = lm.lead_manager_code 
-		left join Manager m on m.senior_manager_code = sm.senior_manager_code
-		left join Employee e on e.manager_code = m.manager_code
-	--order by 
-	--c.company_code
-	--, lm.lead_manager_code
-	--, sm.senior_manager_code
-	--, m.manager_code
-	--, e.employee_code
-)
-, cte2
-as
-(
-	select 
-	company_code
-	, count(distinct lead_manager_code) over(partition by lead_manager_code) lead_manager_code
-	, count(distinct senior_manager_code) over(partition by lead_manager_code) senior_manager_code
-	, count(distinct manager_code) over(partition by lead_manager_code) manager_code
-	, count(distinct employee_code) over(partition by lead_manager_code) employee_code
-	from cte
-	group by company_code
-)
-select * from cte2
+/*Query for generated tables from init-database.sql*/
+/*
+select distinct
+        c.company_code
+        , c.founder
+        , count(distinct lm.lead_manager_code) q1
+        , count(distinct sm.senior_manager_code) q2
+        , count(distinct m.manager_code) q3
+        , count(distinct e.employee_code) q4
+    from TC_Company c
+        left join TC_Lead_Manager lm on lm.company_code = c.company_code
+        left join TC_Senior_Manager sm on sm.lead_manager_code = lm.lead_manager_code 
+        left join TC_Manager m on m.senior_manager_code = sm.senior_manager_code
+        left join TC_Employee e on e.manager_code = m.manager_code
+    group by c.company_code, c.founder
+    order by c.company_code
+*/
 
-
-
-
---;with counts
---as
---(
---	select distinct
---	company_code
---	, count(lead_manager_code) over(partition by lead_manager_code) lead_manager_code
---	, count(senior_manager_code) over(partition by lead_manager_code) senior_manager_code
---	, count(manager_code) over(partition by lead_manager_code) manager_code
---	, count(employee_code) over(partition by lead_manager_code) employee_code
---	from Employee e 
---)
---, summs
---as
---(
---	select distinct
---	company_code
---	, lead_manager_code
---	, sum(senior_manager_code) over(partition by lead_manager_code) senior_manager_code
---	, sum(manager_code) over(partition by senior_manager_code) manager_code
---	, sum(employee_code) over(partition by manager_code) employee_code
---	from counts
---)
---select * from summs
---select distinct
---company_code
---, lead_manager_code
---, senior_manager_code
---, manager_code
---, employee_code
---from Employee e 
+select distinct
+    c.company_code
+    , c.founder
+    , count(distinct lm.lead_manager_code) q1
+    , count(distinct sm.senior_manager_code) q2
+    , count(distinct m.manager_code) q3
+    , count(distinct e.employee_code) q4
+from Company c
+    left join Lead_Manager lm on lm.company_code = c.company_code
+    left join Senior_Manager sm on sm.lead_manager_code = lm.lead_manager_code 
+    left join Manager m on m.senior_manager_code = sm.senior_manager_code
+    left join Employee e on e.manager_code = m.manager_code
+group by c.company_code, c.founder
+order by c.company_code
