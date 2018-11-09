13a40e64b6fb3e03211fd707f150bb9ee37270d6
Dmitry Malikov <malikov-dm@yandex.ru>
2018-11-10 00:10:05 +0500

    s10-basic-statistics challenge not checked on site

--- Statistics/s10-basic-statistics.cs
+++ Statistics/s10-basic-statistics.cs
@@ -7,30 +7,46 @@ namespace Statistics
 {
     class s10_basic_statistics
     {
-        public static void Solve(int n, int[] ar){
-            Console.WriteLine("Printing s10_basic_statistics challenge solution");
+        public static void Solve(int n, int[] ar)
+        {
+            //Console.WriteLine("Printing s10_basic_statistics challenge solution");
             Console.WriteLine("{0:n1}", CalcMean(n, ar));
             Console.WriteLine("{0:n1}", CalcMedian(n, ar));
-            Console.WriteLine("{0:n1}", CalcMode(n, ar));
+            Console.WriteLine("{0:n0}", CalcMode(n, ar));
         }
-        static decimal CalcMean(int n, int[] ar){
+        static decimal CalcMean(int n, int[] ar)
+        {
             decimal res = 0;
-            res = ar.Sum() / n;
+            res = Convert.ToDecimal(ar.Sum()) / n;
             return res;
         }
-        static decimal CalcMedian(int n, int[] ar){
+        static decimal CalcMedian(int n, int[] ar)
+        {
             decimal res = 1;
             Array.Sort(ar);
-            if(n % 2 == 1){
+            if (n % 2 == 1)
+            {
                 res = ar[n / 2];
-            } else {
-                res = (ar[n / 2 - 1] + ar[n / 2]) / 2;
             }
-            return res; 
-        }
-        static decimal CalcMode(int n, int[] ar){
-            decimal res = 2;
+            else
+            {
+                res = Convert.ToDecimal(ar[n / 2 - 1] + ar[n / 2]) / 2;
+            }
             return res;
+        }
+        static int CalcMode(int n, int[] ar)
+        {
+            //int res = 2;
+            var t = ar.GroupBy(v => v)
+                .OrderByDescending(g => g.Count())
+                .ThenBy(g => g.Key).First().Key;
+                //.SelectMany(g => g);
+            //Console.WriteLine(t); //String.Join(" ", t));
+            int mode = ar.GroupBy(v => v)
+                .OrderByDescending(g => g.Count())
+                .ThenBy(g => g.Key)
+                .First().Key;
+            return mode;
         }
     }
 
