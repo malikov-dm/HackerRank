41e5b74268df0d6150cd6935894d7c54ff22cff0
dmalikov <dmalikov@rdvb.ru>
2018-12-09 20:19:00 +0300

    feature branch created

--- 30 Days of Code/Program.cs
+++ 30 Days of Code/Program.cs
@@ -6,28 +6,7 @@ namespace _30_Days_of_Code
     {
         static void Main(string[] args)
         {
-            int i = 4;
-            double d = 4.0;
-            string s = "HackerRank ";
-
-            // Declare second integer, double, and String variables.
-
-            // Read and save an integer, double, and String to your variables.
-
-            // Print the sum of both integer variables on a new line.
-
-            // Print the sum of the double variables on a new line.
-
-            // Concatenate and print the String variables on a new line
-            // The 's' variable above should be printed first.
-
-            int inpi = Convert.ToInt32(Console.ReadLine());
-            double inpd = Convert.ToDouble(Console.ReadLine());
-            string inps = Console.ReadLine();
-
-            Console.WriteLine("{0}", i + inpi);
-            Console.WriteLine("{0:F1}", d + inpd);
-            Console.WriteLine("{0}{1}", s, inps);
+            Console.WriteLine("feature branch");
         }
     }
 }
