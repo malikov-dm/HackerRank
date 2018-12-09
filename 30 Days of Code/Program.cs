ed75d7e18ef5772b12396b63b15404e9ec8300c0
dmalikov <dmalikov@rdvb.ru>
2018-12-09 21:02:55 +0300

    Day 2: Operators

--- 30 Days of Code/Program.cs
+++ 30 Days of Code/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace _30_Days_of_Code
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Задание 3");
-        }
+class Solution {
+
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        var resDouble = meal_cost + meal_cost * tip_percent / 100 + meal_cost * tax_percent / 100;
+        var resInt = Convert.ToInt32(resDouble);
+        Console.WriteLine(resInt);
+    }
+
+    static void Main(string[] args) {
+        double meal_cost = Convert.ToDouble(Console.ReadLine());
+
+        int tip_percent = Convert.ToInt32(Console.ReadLine());
+
+        int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+        solve(meal_cost, tip_percent, tax_percent);
     }
 }
