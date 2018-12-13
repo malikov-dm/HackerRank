b5a0f87d2444468e745af15b0ea61c3e03862689
dmalikov <dmalikov@rdvb.ru>
2018-12-13 10:27:40 +0300

    Day 5: Loops

--- 30 Days of Code/Program.cs
+++ 30 Days of Code/Program.cs
@@ -1,59 +1,28 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-class Person
-{
-    public int age;
-    public Person(int initialAge)
-    {
-        // Add some more code to run some checks on initialAge
-        if (initialAge < 0)
-        {
-            Console.WriteLine("Age is not valid, setting age to 0.");
-            age = 0;
-        }
-        else
-        {
-            age = initialAge;
-        }
-    }
-    public void amIOld()
-    {
-        // Do some computations in here and print out the correct statement to the console 
-        if(age < 13)
-        {
-            Console.WriteLine("You are young.");
-        }
-        else if(age >= 13 && age < 18)
-        {
-            Console.WriteLine("You are a teenager.");
-        }else
-        {
-            Console.WriteLine("You are old.");
-        }
-    }
+class Solution {
 
-    public void yearPasses()
-    {
-        // Increment the age of the person in here
-        age++;
-    }
 
-    static void Main(String[] args)
-    {
-        int T = int.Parse(Console.In.ReadLine());
-        for (int i = 0; i < T; i++)
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
         {
-            int age = int.Parse(Console.In.ReadLine());
-            Person p = new Person(age);
-            p.amIOld();
-            for (int j = 0; j < 3; j++)
-            {
-                p.yearPasses();
-            }
-            p.amIOld();
-            Console.WriteLine();
+            Console.WriteLine($"{n} x {i} = {{0}}", n * i);
+
         }
     }
 }
