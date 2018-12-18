dbfb9794995d57852bb78bd9947d313fbbb2e0cd
dmalikov <dmalikov@rdvb.ru>
2018-12-18 10:55:59 +0300

    Day 10: Binary Numbers

--- 30 Days of Code/Program.cs
+++ 30 Days of Code/Program.cs
@@ -14,28 +14,29 @@ using System;
 
 class Solution
 {
-
-    // Complete the factorial function below.
-    static int factorial(int n)
-    {
-        if (n == 0) return 1;
-
-        if (n == 1) return 1;
-
-        return n * factorial(n - 1);
-    }
-
     static void Main(string[] args)
     {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
         int n = Convert.ToInt32(Console.ReadLine());
+        int currentCons = 0;
+        int maxCons = 0;
+        while (n > 0)
+        {
+            var reminder = n % 2;
+            if (reminder > 0)
+            {
+                currentCons++;
+                if (currentCons > maxCons)
+                {
+                    maxCons = currentCons;
+                }
+            }
+            else
+            {
+                currentCons = 0;
+            }
 
-        int result = factorial(n);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
+            n /= 2;
+        }
+        Console.WriteLine(maxCons);
     }
 }
