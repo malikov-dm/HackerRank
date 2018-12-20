c545aec20113c0e032e209fec3c6069645616796
Dmitriy Malikov <dmalikov@rdvb.ru>
2018-12-20 15:57:55 +0500

    Day 12: Inheritance

--- 30 Days of Code/Program.cs
+++ 30 Days of Code/Program.cs
@@ -1,78 +1,79 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
+﻿using System;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+
+class Person
+{
+    protected string firstName;
+    protected string lastName;
+    protected int id;
+
+    public Person() { }
+    public Person(string firstName, string lastName, int identification)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = identification;
+    }
+    public void printPerson()
+    {
+        Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+    }
+}
+
+class Student : Person
+{
+    private int[] testScores;
+
+    public Student(string firstName, string lastName, int id, int[] scores) : base(firstName, lastName, id)
+    {
+        this.testScores = scores;
+    }
+    
+    public char Calculate()
+    {
+        var average = testScores.Sum() / testScores.Length;
+        if(average <= 100 && average >= 90)
+        {
+            return 'O';
+        } 
+        if(average < 90 && average >= 80)
+        {
+            return 'E';
+        } 
+        if(average < 80 && average >= 70)
+        {
+            return 'A';
+        } 
+        if(average <= 70 && average >= 55)
+        {
+            return 'P';
+        } 
+        if(average < 55 && average >= 40)
+        {
+            return 'D';
+        }         
+        return 'T';
+    }
+}
 
 class Solution
 {
-
-
-
-    static void Main(string[] args)
+    static void Main()
     {
-        int[][] arr = new int[6][];
-
-        for (int i = 0; i < 6; i++)
+        string[] inputs = Console.ReadLine().Split();
+        string firstName = inputs[0];
+        string lastName = inputs[1];
+        int id = Convert.ToInt32(inputs[2]);
+        int numScores = Convert.ToInt32(Console.ReadLine());
+        inputs = Console.ReadLine().Split();
+        int[] scores = new int[numScores];
+        for (int i = 0; i < numScores; i++)
         {
-            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            scores[i] = Convert.ToInt32(inputs[i]);
         }
 
-        int maxSum = Int32.MinValue, currSum = 0;
-
-        int x0 = 0, y0 = 0;
-
-
-        bool flag = true;
-
-        while (flag)
-        {
-            
-            if (x0 + 2 < 6)
-            {
-                //Console.WriteLine($"{arr[y0][x0]} {arr[y0][x0 + 1]} {arr[y0][x0 + 2]}");
-                //Console.WriteLine($"  {arr[y0 + 1][x0 + 1]}  ");
-                //Console.WriteLine($"{arr[y0 + 2][x0]} {arr[y0 + 2][x0 + 1]} {arr[y0 + 2][x0 + 2]}");
-                currSum = arr[y0][x0] + arr[y0][x0 + 1] + arr[y0][x0 + 2] + arr[y0 + 1][x0 + 1] + arr[y0 + 2][x0] + arr[y0 + 2][x0 + 1] + arr[y0 + 2][x0 + 2];
-                //Console.WriteLine();
-                //Console.WriteLine(currSum);
-                //Console.WriteLine();
-                maxSum = currSum > maxSum ? currSum : maxSum;
-                x0++;
-            }
-            else
-            {
-                x0 = 0;
-                y0++;
-            }
-
-            //Console.WriteLine($"x0 = {x0}, y0 = {y0}");
-
-
-            if (y0 + 2 >= 6) flag = false;
-
-
-        }
-
-        Console.WriteLine(maxSum);
-
-        /* for (int x = 0; x < 6; x++)
-        {
-            for (int y = 0; y < 6; y++)
-            {
-                Console.Write($"{arr[x][y]} ");
-            }
-
-            Console.WriteLine();
-        } */
-
+        Student s = new Student(firstName, lastName, id, scores);
+        s.printPerson();
+        Console.WriteLine("Grade: " + s.Calculate() + "\n");
     }
 }
