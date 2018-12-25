3ae7309bc866194d56dfb1387ff9b0fa640f6dac
Dmitriy Malikov <dmalikov@rdvb.ru>
2018-12-25 19:48:55 +0500

    Day 16: Exceptions - String to Integer

--- 30 Days of Code/Program.cs
+++ 30 Days of Code/Program.cs
@@ -1,54 +1,19 @@
 ﻿using System;
-class Node
-{
-    public int data;
-    public Node next;
-    public Node(int d)
-    {
-        data = d;
-        next = null;
-    }
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
 
-}
-class Solution
-{
-
-    public static Node insert(Node head, int data)
-    {
-        if (head == null)
+    static void Main(String[] args) {
+        string S = Console.ReadLine();
+        try
         {
-            return new Node(data);
-        }
-        else if (head.next == null)
+            Console.WriteLine(Convert.ToInt32(S));    
+        } 
+        catch
         {
-            head.next = new Node(data);
+            Console.WriteLine("Bad String");
         }
-        else
-        {
-            insert(head.next, data);
-        }
-        return head;
-    }
-
-    public static void display(Node head)
-    {
-        Node start = head;
-        while (start != null)
-        {
-            Console.Write(start.data + " ");
-            start = start.next;
-        }
-    }
-    static void Main(String[] args)
-    {
-
-        Node head = null;
-        int T = Int32.Parse(Console.ReadLine());
-        while (T-- > 0)
-        {
-            int data = Int32.Parse(Console.ReadLine());
-            head = insert(head, data);
-        }
-        display(head);
     }
 }
+
