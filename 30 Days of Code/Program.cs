61eae1be123d605b61f62666efe46bcb172a511a
Dmitriy Malikov <dmalikov@rdvb.ru>
2018-12-25 19:59:16 +0500

    Day 17: More Exceptions

--- 30 Days of Code/Program.cs
+++ 30 Days of Code/Program.cs
@@ -2,18 +2,46 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-class Solution {
 
-    static void Main(String[] args) {
-        string S = Console.ReadLine();
-        try
+internal class Calculator
+{
+    internal int power(int p, int n)
+    {
+        if (p < 0 || n < 0)
         {
-            Console.WriteLine(Convert.ToInt32(S));    
-        } 
-        catch
-        {
-            Console.WriteLine("Bad String");
+            throw new Exception("n and p should be non-negative");
         }
+        return (int)Math.Pow(p, n);
+    }
+}
+
+//Write your code here
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        Calculator myCalculator = new Calculator();
+        int T = Int32.Parse(Console.ReadLine());
+        while (T-- > 0)
+        {
+            string[] num = Console.ReadLine().Split();
+            int n = int.Parse(num[0]);
+            int p = int.Parse(num[1]);
+            try
+            {
+                int ans = myCalculator.power(n, p);
+                Console.WriteLine(ans);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+
+
+
     }
 }
 
