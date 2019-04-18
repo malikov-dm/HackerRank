4035a1028ea758023786a7fd5d959cbd5e2edd3e
Dmitriy Malikov <dmalikov@rdvb.ru>
2019-04-18 11:43:24 +0500

    ServiceTitan initial commit

---
+++ ServiceTitan/ServiceTitan.ProblemOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTitan.ProblemOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
