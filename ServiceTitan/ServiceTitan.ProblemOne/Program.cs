34a2fd5ecfbabb84c84e3d6c845ad99849cb0085
Dmitriy Malikov <dmalikov@rdvb.ru>
2019-04-20 21:22:32 +0500

    Все кроме рекурсии

--- ServiceTitan/ServiceTitan.ProblemOne/Program.cs
+++ ServiceTitan/ServiceTitan.ProblemOne/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Challenges.ArrayExtensions;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -6,7 +8,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-namespace ServiceTitan.ProblemOne
+namespace Challenges
 {
     public interface ICloningService
     {
@@ -18,14 +20,204 @@ namespace ServiceTitan.ProblemOne
         public T Clone<T>(T source)
         {
             // Please implement this method
-            // throw new NotImplementedException();
-
             return source.Copy();
         }
 
         // Feel free to add any other methods, classes, etc.
     }
 
+    public static class ObjectExtensions
+    {
+        private static readonly MethodInfo CloneMethod = typeof(Object).GetMethod("MemberwiseClone", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        public static bool IsPrimitive(this Type type)
+        {
+            if (type == typeof(String)) return true;
+            return (type.IsValueType & type.IsPrimitive);
+        }
+
+        public static Object Copy(this Object originalObject)
+        {
+            return InternalCopy(originalObject, new Dictionary<Object, Object>(new ReferenceEqualityComparer()));
+        }
+        private static Object InternalCopy(Object originalObject, IDictionary<Object, Object> visited)
+        {
+
+            if (originalObject == null) return null;
+            var typeToReflect = originalObject.GetType();
+            if (IsPrimitive(typeToReflect)) return originalObject;
+            if (visited.ContainsKey(originalObject)) return visited[originalObject];
+            if (typeof(Delegate).IsAssignableFrom(typeToReflect)) return null;
+
+            //пройтись во всем мемберам и в зависимости от атрибута сделать их копии
+
+            var cloneObject = CloneMethod.Invoke(originalObject, null);
+            if (typeToReflect.IsArray)
+            {
+                var arrayType = typeToReflect.GetElementType();
+                if (IsPrimitive(arrayType) == false)
+                {
+                    Array clonedArray = (Array)cloneObject;
+                    clonedArray.ForEach((array, indices) => array.SetValue(InternalCopy(clonedArray.GetValue(indices), visited), indices));
+                }
+
+            }
+            visited.Add(originalObject, cloneObject);
+            //CopyFields(originalObject, visited, cloneObject, typeToReflect);
+            CopyProperties(originalObject, visited, cloneObject, typeToReflect);
+            //RecursiveCopyBaseTypePrivateFields(originalObject, visited, cloneObject, typeToReflect);
+            return cloneObject;
+        }
+
+        private static void RecursiveCopyBaseTypePrivateFields(object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect)
+        {
+            if (typeToReflect.BaseType != null)
+            {
+                RecursiveCopyBaseTypePrivateFields(originalObject, visited, cloneObject, typeToReflect.BaseType);
+                CopyFields(originalObject, visited, cloneObject, typeToReflect.BaseType, BindingFlags.Instance | BindingFlags.NonPublic, info => info.IsPrivate);
+            }
+        }
+
+        private static void CopyFields(object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy, Func<FieldInfo, bool> filter = null)
+        {
+            foreach (FieldInfo fieldInfo in typeToReflect.GetFields(bindingFlags))
+            {
+                if (filter != null && filter(fieldInfo) == false) continue;
+                if (IsPrimitive(fieldInfo.FieldType)) continue;
+                var originalFieldValue = fieldInfo.GetValue(originalObject);
+                var clonedFieldValue = InternalCopy(originalFieldValue, visited);
+                fieldInfo.SetValue(cloneObject, clonedFieldValue);
+            }
+        }
+        private static void CopyProperties(object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy, Func<PropertyInfo, bool> filter = null)
+        {
+            if (typeToReflect.IsArray || typeToReflect.IsEnumerableType()) return;
+            foreach (PropertyInfo propertyInfo in typeToReflect.GetProperties(bindingFlags))
+            {
+                if (filter != null && filter(propertyInfo) == false) continue;
+                var attr = propertyInfo.GetCustomAttribute(typeof(CloneableAttribute));
+                if (attr is CloneableAttribute a)
+                {
+                    switch (a.Mode)
+                    {
+                        case CloningMode.Ignore:
+                            {
+                                propertyInfo.SetValue(cloneObject, null);
+                                break;
+                            }
+                        case CloningMode.Shallow:
+                            {
+                                propertyInfo.SetValue(cloneObject, propertyInfo.GetValue(originalObject));
+                                break;
+                            }
+                        case CloningMode.Deep:
+                        default:
+                            {
+                                var originalPropertyValue = propertyInfo.GetValue(originalObject);
+                                var clonedPropertyValue = InternalCopy(originalPropertyValue, visited);
+                                propertyInfo.SetValue(cloneObject, clonedPropertyValue);
+                                break;
+                            }
+                    }
+                    continue;
+                }
+                if (IsPrimitive(propertyInfo.PropertyType)) continue;
+                //var originalPropertyValue = propertyInfo.GetValue(originalObject);
+                //var clonedPropertyValue = InternalCopy(originalPropertyValue, visited);
+                //propertyInfo.SetValue(cloneObject, clonedPropertyValue);
+            }
+        }
+        public static T Copy<T>(this T original)
+        {
+            return (T)Copy((Object)original);
+        }
+
+        public static CloneableAttribute GetFieldAttr<T>(this T source, Expression<Func<T, object>> field)
+        {
+            var member = field.Body as MemberExpression;
+            if (member == null) return null; // or throw exception
+
+            var fieldName = member.Member.Name;
+
+            var test = typeof(T);
+            var fieldType = test.GetField(fieldName);
+            if (fieldType != null)
+            {
+                var attribute = fieldType.GetCustomAttribute<CloneableAttribute>();
+
+                return attribute;
+            }
+
+            return null;
+        }
+
+        private static bool IsEnumerableType(this Type type)
+        {
+            return (type.GetInterface(nameof(IEnumerable)) != null);
+        }
+    }
+
+    public class ReferenceEqualityComparer : EqualityComparer<Object>
+    {
+        public override bool Equals(object x, object y)
+        {
+            return ReferenceEquals(x, y);
+        }
+        public override int GetHashCode(object obj)
+        {
+            if (obj == null) return 0;
+            return obj.GetHashCode();
+        }
+    }
+
+
+
+    namespace ArrayExtensions
+    {
+        public static class ArrayExtensions
+        {
+            public static void ForEach(this Array array, Action<Array, int[]> action)
+            {
+                if (array.LongLength == 0) return;
+                ArrayTraverse walker = new ArrayTraverse(array);
+                do action(array, walker.Position);
+                while (walker.Step());
+            }
+        }
+
+        internal class ArrayTraverse
+        {
+            public int[] Position;
+            private int[] maxLengths;
+
+            public ArrayTraverse(Array array)
+            {
+                maxLengths = new int[array.Rank];
+                for (int i = 0; i < array.Rank; ++i)
+                {
+                    maxLengths[i] = array.GetLength(i) - 1;
+                }
+                Position = new int[array.Rank];
+            }
+
+            public bool Step()
+            {
+                for (int i = 0; i < Position.Length; ++i)
+                {
+                    if (Position[i] < maxLengths[i])
+                    {
+                        Position[i]++;
+                        for (int j = 0; j < i; j++)
+                        {
+                            Position[j] = 0;
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
     public enum CloningMode
     {
         Deep = 0,
@@ -121,7 +313,7 @@ namespace ServiceTitan.ProblemOne
             sw.Start();
             test();
             sw.Stop();
-            Console.WriteLine($"{title}: {sw.Elapsed.TotalMilliseconds:0.000}ms");
+            // Console.WriteLine($"{title}: {sw.Elapsed.TotalMilliseconds:0.000}ms");
         }
 
         public void SimpleTest()
@@ -175,7 +367,6 @@ namespace ServiceTitan.ProblemOne
         {
             var s = new Node();
             s.Left = s;
-            //var cnt = s.TotalNodeCount;
             var c = Cloner.Clone(s);
             Assert(s != c);
             Assert(null == c.Right);
@@ -289,8 +480,15 @@ namespace ServiceTitan.ProblemOne
         {
             foreach (var test in AllTests)
             {
-                test.Invoke();
-                Console.WriteLine($"{test.Method.Name} passed");
+                try
+                {
+                    test.Invoke();
+                    Console.WriteLine($"{test.GetMethodInfo().Name} passed.");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Failed on {test.GetMethodInfo().Name}.");
+                }
             }
             Console.WriteLine("Done.");
         }
@@ -301,8 +499,8 @@ namespace ServiceTitan.ProblemOne
         public static void Main(string[] args)
         {
             var cloningServiceTest = new CloningServiceTest();
-            //cloningServiceTest.RunAllTests();
             var allTests = cloningServiceTest.AllTests;
+            cloningServiceTest.RunAllTests();
             while (true)
             {
                 var line = Console.ReadLine();
@@ -313,7 +511,7 @@ namespace ServiceTitan.ProblemOne
                 {
                     test.Invoke();
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
                     Console.WriteLine($"Failed on {test.GetMethodInfo().Name}.");
                 }
--- ServiceTitan/ServiceTitan.ProblemThree/Program.cs
+++ ServiceTitan/ServiceTitan.ProblemThree/Program.cs
@@ -1,29 +1,205 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace ServiceTitan.ProblemThree
+namespace Solution
 {
-    class Program
+    public interface IPushToPullStream : IDisposable
     {
-        static void Main(string[] args)
-        {
-            var s = new Simple() { I = 5, S = "eqwe" };
-            var res = s.Computed;
+        Stream PushStream { get; }
+        Stream PullStream { get; }
+        void CompleteWrite();
+    }
+}
 
-            Console.WriteLine(res);
-            Console.ReadLine();
+namespace Solution
+{
+    public abstract class StreamBase : Stream
+    {
+        protected long _position;
+        public override bool CanSeek { get; } = false;
+        public override bool CanRead { get; } = false;
+        public override bool CanWrite { get; } = false;
+        public override long Length { get; }
+
+        public override long Position
+        {
+            get => _position;
+            set
+            {
+                if (_position != value) throw new NotSupportedException();
+            }
+        }
+
+        public override void Flush() { }
+        public override long Seek(long offset, SeekOrigin origin) =>
+            throw new System.NotSupportedException();
+        public override void SetLength(long value) =>
+            throw new System.NotSupportedException();
+        public override int Read(byte[] buffer, int offset, int count) =>
+            throw new NotSupportedException();
+        public override void Write(byte[] buffer, int offset, int count) =>
+            throw new NotSupportedException();
+    }
+
+    public class PushStreamImpl : StreamBase
+    {
+        public PushToPullStream Owner { get; }
+        public override bool CanWrite => true;
+
+        public PushStreamImpl(PushToPullStream owner) => Owner = owner;
+
+        public override void Write(byte[] buffer, int offset, int count) =>
+            Task.Run(() => WriteAsync(buffer, offset, count, CancellationToken.None)).Wait();
+
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            // Implement this method
+            throw new NotImplementedException();
         }
     }
 
-    class Simple
+    public class PullStreamImpl : StreamBase
     {
-        public string S { get; set; }
-        public int I { get; set; }
+        public PushToPullStream Owner { get; }
+        public override bool CanRead => true;
 
-        public string Computed => S + I;
+        public PullStreamImpl(PushToPullStream owner) => Owner = owner;
 
+        public override int Read(byte[] buffer, int offset, int count) =>
+            Task.Run(() => ReadAsync(buffer, offset, count, CancellationToken.None)).Result;
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count,
+            CancellationToken cancellationToken)
+        {
+            // Implement this method
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PushToPullStream : IPushToPullStream
+    {
+        public const int DefaultBufferSize = 8192;
+
+        // You may declare the buffer elsewhere, but the important part about it is:
+        // you should design your impl. in such a way this buffer is never exceeded.
+        // I.e. if a writer (the code using PushStream) tries to write more data you
+        // can actually buffer, it has to wait.
+        public byte[] Buffer { get; set; }
+        public Stream PushStream { get; protected set; }
+        public Stream PullStream { get; protected set; }
+
+        public PushToPullStream(int bufferSize = DefaultBufferSize)
+        {
+            Buffer = new byte[bufferSize];
+            PushStream = new PushStreamImpl(this);
+            PullStream = new PullStreamImpl(this);
+        }
+
+        public virtual void CompleteWrite()
+        {
+            // Implement this method
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            PushStream?.Dispose();
+            PushStream = null;
+            PullStream?.Dispose();
+            PullStream = null;
+        }
+    }
+}
+
+namespace Solution
+{
+    public class Solution
+    {
+        public static void Main()
+        {
+            var tester = new PushToPullStreamTester()
+            {
+                PushToPullFactory = () => new PushToPullStream(100)
+            };
+            tester.Test();
+            Console.WriteLine("Test passed.");
+        }
+    }
+
+    public class PushToPullStreamTester
+    {
+        public Func<IPushToPullStream> PushToPullFactory { get; set; }
+        public Random Random1 { get; set; } = new Random(12345);
+        public Random Random2 { get; set; } = new Random(54321);
+
+        public void Test()
+        {
+            for (var l = 0; l < 100; l++)
+                for (var s = 1; s <= 8; s <<= 1)
+                    TestOne(l, s);
+            TestOne(20000000, 8192, 0.001, 0);
+            TestOne(20000000, 8192, 0, 0.001);
+        }
+
+        private void TestOne(long length, int maxChunkLength = 1024,
+            double pushDelayProbability = 0,
+            double pullDelayProbability = 0,
+            int delayDurationInMilliseconds = 50)
+        {
+            using (var p2p = PushToPullFactory.Invoke())
+            {
+                long inputCrc = 0;
+                long outputCrc = 0;
+
+                var writerTask = Task.Run(async () => {
+                    var buffer = new byte[maxChunkLength];
+                    while (p2p.PushStream.Position < length)
+                    {
+                        var chunkSize = (int)Math.Min(
+                            Random1.Next(1 + maxChunkLength),
+                            length - p2p.PushStream.Position);
+                        Random1.NextBytes(buffer);
+                        AccumulateCrc(buffer, buffer.Length, ref inputCrc);
+                        if (Random1.NextDouble() < pushDelayProbability)
+                            await Task.Delay(delayDurationInMilliseconds);
+                        await p2p.PushStream.WriteAsync(buffer, 0, buffer.Length);
+                    }
+                    p2p.CompleteWrite();
+                });
+                var readerTask = Task.Run(async () => {
+                    var buffer = new byte[maxChunkLength];
+                    var readLength = 1;
+                    while (readLength > 0)
+                    {
+                        var chunkSize = 1 + Random2.Next(maxChunkLength);
+                        if (Random1.NextDouble() < pullDelayProbability)
+                            await Task.Delay(delayDurationInMilliseconds);
+                        readLength = await p2p.PullStream.ReadAsync(buffer, 0, buffer.Length);
+                        AccumulateCrc(buffer, readLength, ref outputCrc);
+                    }
+                });
+
+                Task.WaitAll(writerTask, readerTask);
+                if (inputCrc != outputCrc)
+                    throw new InvalidOperationException("CRC check failed.");
+            }
+        }
+
+        private void AccumulateCrc(byte[] buffer, int length, ref long crc)
+        {
+            unchecked
+            {
+                var c = crc;
+                for (var i = 0; i < length; i++)
+                {
+                    var x = buffer[i];
+                    c = 271 * c + x;
+                }
+
+                crc = c;
+            }
+        }
     }
 }
